2ab850aabab4b1add33c5d139971a49299c29bf5
Aditya Solanki <adityasolanki@Adis-MacBook-Air.local>
2024-06-21 02:05:06 +0530

    add navbar and issues page

---
+++ app/NavBar.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { Bug } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export default function NavBar() {
+    const pathName = usePathname();
+    const Links = [
+        {
+            href: '/',
+            label: 'Dashboard',
+        },
+        {
+            href: '/issues',
+            label: 'Issues',
+        },
+    ];
+
+    return (
+        <nav className="flex space-x-8 h-14 items-center pl-8 border-b font-medium">
+            <Link href="/">
+                <Bug />
+            </Link>
+            <ul className="flex space-x-8 ">
+                {Links.map((link) => (
+                    <Link
+                        key={link.href}
+                        href={link.href}
+                        className={`${
+                            pathName === link.href
+                                ? 'text-zinc-900'
+                                : 'text-zinc-500'
+                        } hover:text-zinc-900 transition-colors`}
+                    >
+                        {link.label}
+                    </Link>
+                ))}
+            </ul>
+        </nav>
+    );
+}
