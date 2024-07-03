827b640c6ff6e81352bf635c449bc596a4216b8a
Aditya Solanki <adityasolanki@Adis-MacBook-Air.local>
2024-07-03 16:04:08 +0530

    handle error on deletion

--- app/issues/[id]/edit/_components/DeleteIssueButton.tsx
+++ app/issues/[id]/edit/_components/DeleteIssueButton.tsx
@@ -21,15 +21,18 @@ import { useState } from 'react';
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
     const router = useRouter();
     const [isDeleting, setIsDeleting] = useState(false);
+    const [error, setError] = useState(false);
 
     const deleteIssue = async () => {
         try {
+            throw new Error('This is a test error');
             setIsDeleting(true);
             await axios.delete('/api/issues/' + issueId);
             router.push('/issues');
             router.refresh();
         } catch (error) {
             setIsDeleting(false);
+            setError(true);
             console.error(error);
         }
     };
@@ -43,30 +46,53 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
             {isDeleting ? (
                 <Spinner />
             ) : (
-                <AlertDialog>
-                    <AlertDialogTrigger>Delete Issue</AlertDialogTrigger>
-                    <AlertDialogContent>
-                        <AlertDialogHeader>
-                            <AlertDialogTitle>
-                                Are you absolutely sure?
-                            </AlertDialogTitle>
-                            <AlertDialogDescription>
-                                This action cannot be undone. This will
-                                permanently delete your issue and remove your
-                                data from our servers.
-                            </AlertDialogDescription>
-                        </AlertDialogHeader>
-                        <AlertDialogFooter>
-                            <AlertDialogCancel>Cancel</AlertDialogCancel>
-                            <AlertDialogAction
-                                className="bg-destructive hover:bg-destructive"
-                                onClick={deleteIssue}
-                            >
-                                Delete Issue
-                            </AlertDialogAction>
-                        </AlertDialogFooter>
-                    </AlertDialogContent>
-                </AlertDialog>
+                <>
+                    <AlertDialog>
+                        <AlertDialogTrigger>Delete Issue</AlertDialogTrigger>
+                        <AlertDialogContent>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>
+                                    Are you absolutely sure?
+                                </AlertDialogTitle>
+                                <AlertDialogDescription>
+                                    This action cannot be undone. This will
+                                    permanently delete your issue and remove
+                                    your data from our servers.
+                                </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                <AlertDialogAction
+                                    className="bg-destructive hover:bg-destructive"
+                                    onClick={deleteIssue}
+                                >
+                                    Delete Issue
+                                </AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
+                    <AlertDialog open={error}>
+                        <AlertDialogContent>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>
+                                    An error occurred
+                                </AlertDialogTitle>
+                                <AlertDialogDescription>
+                                    There was an error deleting your issue. Please
+                                    try again later.
+                                </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                                <AlertDialogAction
+                                    className="bg-destructive hover:bg-destructive"
+                                    onClick={() => setError(false)}
+                                >
+                                    Ok
+                                </AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
+                </>
             )}
         </Button>
     );
