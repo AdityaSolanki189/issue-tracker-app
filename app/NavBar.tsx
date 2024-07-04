c78bac34b9dfda7a3fe2c86e86d1144d76822166
Aditya Solanki <adityasolanki@Adis-MacBook-Air.local>
2024-07-05 00:03:10 +0530

    refactor: navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,9 +1,6 @@
 'use client';
 
-import { Bug } from 'lucide-react';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { useSession } from 'next-auth/react';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -12,12 +9,31 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Session } from 'inspector';
+import { Skeleton } from '@/components/ui/skeleton';
+import { Bug } from 'lucide-react';
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function NavBar() {
+    return (
+        <nav className="flex h-14 items-center pl-8 border-b font-medium justify-between">
+            <div className="flex space-x-8">
+                <Link href="/">
+                    <Bug />
+                </Link>
+                <NavLinks />
+            </div>
+
+            <div className=" ml-auto flex space-x-8 items-center pr-8 ">
+                <AuthStatus />
+            </div>
+        </nav>
+    );
+}
+
+const NavLinks = () => {
     const pathName = usePathname();
-    const { status, data: session } = useSession();
 
     const Links = [
         {
@@ -31,61 +47,58 @@ export default function NavBar() {
     ];
 
     return (
-        <nav className="flex h-14 items-center pl-8 border-b font-medium justify-between">
-            <div className="flex space-x-8">
-                <Link href="/">
-                    <Bug />
-                </Link>
-                <ul className="flex space-x-8 ">
-                    {Links.map((link) => (
-                        <li key={link.href}>
-                            <Link
-                                href={link.href}
-                                className={`${
-                                    pathName === link.href
-                                        ? 'text-slate-900 font-semibold underline'
-                                        : 'text-slate-500'
-                                } hover:text-zinc-900 transition-colors`}
-                            >
-                                {link.label}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-
-            <div className=" ml-auto flex space-x-8 items-center pr-8 ">
-                {status === 'authenticated' ? (
-                    <div>
-                        {/* <Link href="/api/auth/signout">Sign Out</Link> */}
-                        <DropdownMenu>
-                            <DropdownMenuTrigger>
-                                <button className="text-slate-500 hover:text-zinc-900 transition-colors">
-                                    <Avatar>
-                                        <AvatarImage
-                                            src={session.user!.image!}
-                                        />
-                                        <AvatarFallback>CN</AvatarFallback>
-                                    </Avatar>
-                                </button>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent>
-                                <DropdownMenuLabel>{session.user!.email}</DropdownMenuLabel>
-                                <DropdownMenuSeparator />
-                                <DropdownMenuItem>
-                                    <Link href="/api/auth/signout">
-                                        Sign Out
-                                    </Link>
-                                </DropdownMenuItem>
-                            </DropdownMenuContent>
-                        </DropdownMenu>
-                    </div>
-                ) : (
-                    <div>
-                        <Link href="/api/auth/signin">Sign In</Link>
-                    </div>
-                )}
-            </div>
-        </nav>
+        <ul className="flex space-x-8 ">
+            {Links.map((link) => (
+                <li key={link.href}>
+                    <Link
+                        href={link.href}
+                        className={`${
+                            pathName === link.href
+                                ? 'text-slate-900 font-semibold underline'
+                                : 'text-slate-500'
+                        } hover:text-zinc-900 transition-colors`}
+                    >
+                        {link.label}
+                    </Link>
+                </li>
+            ))}
+        </ul>
     );
-}
+};
+
+const AuthStatus = () => {
+    const { status, data: session } = useSession();
+
+    if (status === 'loading') {
+        return <Skeleton className="w-[70px] h-[40px] rounded-full" />
+    }
+
+    if (status === 'unauthenticated') {
+        return <Link href="/api/auth/signin">Sign In</Link>;
+    }
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <button className="text-slate-500 hover:text-zinc-900 transition-colors">
+                    <Avatar>
+                        <AvatarImage
+                            src={session!.user!.image!}
+                            referrerPolicy="no-referrer"
+                        />
+                        <AvatarFallback>
+                            {session!.user!.name![0]}
+                        </AvatarFallback>
+                    </Avatar>
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>{session!.user!.email}</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                    <Link href="/api/auth/signout">Sign Out</Link>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
