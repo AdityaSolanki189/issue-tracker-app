040f0a4769a8d5927e6c3d3dd23e4ad98e885110
Aditya Solanki <adityasolanki@Adis-MacBook-Air.local>
2024-07-04 12:17:38 +0530

    add login and logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,9 +3,12 @@
 import { Bug } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 
 export default function NavBar() {
     const pathName = usePathname();
+    const { status, data: session } = useSession();
+
     const Links = [
         {
             href: '/',
@@ -24,19 +27,40 @@ export default function NavBar() {
             </Link>
             <ul className="flex space-x-8 ">
                 {Links.map((link) => (
-                    <Link
-                        key={link.href}
-                        href={link.href}
-                        className={`${
-                            pathName === link.href
-                                ? 'text-slate-900 font-semibold underline'
-                                : 'text-slate-500'
-                        } hover:text-zinc-900 transition-colors`}
-                    >
-                        {link.label}
-                    </Link>
+                    <li key={link.href}>
+                        <Link
+                            href={link.href}
+                            className={`${
+                                pathName === link.href
+                                    ? 'text-slate-900 font-semibold underline'
+                                    : 'text-slate-500'
+                            } hover:text-zinc-900 transition-colors`}
+                        >
+                            {link.label}
+                        </Link>
+                    </li>
                 ))}
             </ul>
+
+            <div
+                className="
+                ml-auto
+                flex
+                space-x-8
+                items-center
+                pr-8
+            "
+            >
+                {status === 'authenticated' ? (
+                    <div>
+                        <Link href="/api/auth/signout">Sign Out</Link>
+                    </div>
+                ) : (
+                    <div>
+                        <Link href="/api/auth/signin">Sign In</Link>
+                    </div>
+                )}
+            </div>
         </nav>
     );
 }
