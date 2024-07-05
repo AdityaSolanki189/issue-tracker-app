5c86710e8af7bd5a98e797784b96b78264d3dde9
Aditya Solanki <adityasolanki@Adis-MacBook-Air.local>
2024-07-05 23:54:06 +0530

    refactor: assignee select component

--- app/issues/_components/AssigneeSelect.tsx
+++ app/issues/_components/AssigneeSelect.tsx
@@ -15,16 +15,7 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-    const {
-        data: users,
-        error,
-        isLoading,
-    } = useQuery<User[]>({
-        queryKey: ['users'],
-        queryFn: () => axios.get('/api/users').then((res) => res.data),
-        staleTime: 1000 * 60,
-        retry: 2,
-    });
+    const { data: users, error, isLoading } = useUsers();
 
     if (isLoading) {
         return <Skeleton />;
@@ -34,22 +25,27 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
         return null;
     }
 
+    const assignIssue = (userId: string) => {
+        axios
+            .patch(`/api/issues/${issue.id}`, {
+                assignedToUserId: userId,
+            })
+            .catch(() => {
+                toast({
+                    title: 'Failed to assign user',
+                    description:
+                        'Changes could not be saved, Please try again later.',
+                    variant: 'destructive',
+                });
+            });
+    };
+
     return (
         <div className="flex gap-2 flex-col max-w-fit">
             <label htmlFor="type-select">Assigned to</label>
             <Select
                 defaultValue={issue.assignedToUserId || '0'}
-                onValueChange={(userId) => {
-                    axios.patch(`/api/issues/${issue.id}`, {
-                        assignedToUserId: userId === '0' ? null : userId,
-                    }).catch(() => {
-                        toast({
-                            title: 'Failed to assign user',
-                            description: 'Changes could not be saved, Please try again later.',
-                            variant: 'destructive',
-                        });
-                    });
-                }}
+                onValueChange={assignIssue}
             >
                 <SelectTrigger id="type-select" className="w-[180px]">
                     <SelectValue placeholder="Assign to..." />
@@ -68,6 +64,15 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
             </Select>
         </div>
     );
+};
+
+const useUsers = () => {
+    return useQuery<User[]>({
+        queryKey: ['users'],
+        queryFn: () => axios.get('/api/users').then((res) => res.data),
+        staleTime: 1000 * 60 * 5,
+        retry: 2,
+    });
 };
 
 export default AssigneeSelect;
