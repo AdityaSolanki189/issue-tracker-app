4ccf1a6994d3edb6210382326a8824072a3527cc
Aditya Solanki <aditya2ss283@gmail.com>
2024-07-08 22:53:56 +0530

    import fix 2

--- app/_components/index.ts
+++ app/_components/index.ts
@@ -1,5 +1,5 @@
 export { default as ErrorMessage } from './error-message';
-export { default as Spinner } from './spinner';
+export { default as Spinner } from './Spinner';
 export { default as StatusBadge } from './status-badge';
 export { default as DataTable } from './data-table';
 export { default as columns } from './columns';
